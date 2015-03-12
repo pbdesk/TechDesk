6e5433448a24a7b9b1518c5497af47be1b531f35
Pinal Bhatt <pinal.bhatt@weightwatchers.com>
2015-03-12 14:11:02 -0400

    TechDesk.Core Module

---
+++ ngApps/Core/TechDesk.Core.config.js
@@ -0,0 +1,18 @@
+﻿(function () {
+	'use strict';
+
+	var coreApp = angular.module('PBDesk.TechDesk.Core');
+
+	//var appUrl = window.c9Configs.appUrl;
+	//var idSysUrl = window.c9Configs.idSysUrl;
+	coreApp.constant('AppSettings', {
+	    ////apiServiceBaseUrl: idSysUrl + 'IdSys/api/',
+		////clientBaseUrl: appUrl,
+		////clientId: window.c9Configs.clientId,
+		////loginUrl: '/Login',
+		////allowedRole: window.c9Configs.allowedRole
+		
+	});
+
+
+})();
---
+++ ngApps/Core/TechDesk.Core.module.js
@@ -0,0 +1,18 @@
+﻿(function () {
+	'use strict';
+
+	var coreApp = angular.module('PBDesk.TechDesk.Core', [
+		'ngAnimate',  'ngMaterial',
+		'jmdobry.angular-cache', 'angular-loading-bar', 'ui.router', 
+		 'PBDesk.Logger', 'PBDesk.Exception'
+	]);
+				   
+
+	
+
+  //  coreApp.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
+  //  	cfpLoadingBarProvider.includeSpinner = true;
+  //  	cfpLoadingBarProvider.includeBar = true;
+  //}])
+
+})();
